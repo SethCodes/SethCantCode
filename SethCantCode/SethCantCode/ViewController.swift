90b8a64cca741152cf0d4477c18ec7ca5f1db275
sethCantCode <56614509+sethCantCode@users.noreply.github.com>
2020-02-11 12:53:50 -0500

    Add

---
+++ SethCantCode/SethCantCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SethCantCode
+//
+//  Created by Seth Stephens on 2/11/20.
+//  Copyright © 2020 Seth Stephens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
